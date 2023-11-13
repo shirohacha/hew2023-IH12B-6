f96bcf214c5d48f360f4cdd9ff46ee778c7bc977
shima <shimadak2003@gmail.com>
2023-11-13 10:25:58 +0900

    不要な空白を削除

--- dbconnect.php
+++ dbconnect.php
@@ -3,6 +3,6 @@ try {
     // 第2,3引数にphpmyadminのユーザー名、パスワードを入力
     $db = new PDO('mysql:dbname=hew2023;host=127.0.0.1;charset=utf8mb4', 'root', '');
 }   catch (PDOException $e) {
-    echo "データベース接続エラー　：".$e->getMessage();
+    echo "データベース接続エラー：".$e->getMessage();
 }
 ?>
